7c6d51bae97f0c373808596b15ebd646ca47fc1c
Grant <GrantWils.23@googlemail.com>
2021-04-09 16:01:44 +0100

    Adding Arsenal invincibles and current team

---
+++ Arsenal_Inv_Players.py
@@ -0,0 +1,6 @@
+import random
+
+Arsenal_Inv_Players = ['Freddie Ljungberg', 'Sol_Campbell', 'Jens Lehmann', 'Ashley Cole', 'Robert Pires', 'Patrick Vieira', 'Kanu', 'Kolo Toure', 'Lauren', 'Thierry Henry',
+                       'Edu', 'Gilberto Silva', 'Martin Keown', 'Jose Reyes', 'Sylvain Wiltord', 'Ray Parlour', 'Pascal Cygan', 'Gael Clichy' 'Jeremie Aliadiere' 'Justin Hoyte', 'David Bentley']
+
+print(random.choice(Arsenal_Inv_Players))
